7a33e6ab3ab8035712c04741f7a7d7c156f38466
Toktam <ttmohebbi@gmail.com>
2021-06-26 03:51:23 +0000

    solve linked lists without using class and is not passing

---
+++ 0002.add-two-numbers.js
@@ -0,0 +1,59 @@
+/*
+2. Add Two Numbers
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+2 ----> 4 ----> 3
+5 ----> 6 ----> 4
+-----------------
+7 ----> 0 ----> 8
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+  let twoSum = Number(l1.reverse().join("")) + Number(l2.reverse().join(""));
+  //.map(Number) converts the array of string numbers! ie ['7', '0', '8']
+  //into an array of numbers ie [7, 0, 8]
+  let results = String(twoSum).split("").reverse().map(Number);
+  return results;
+};
+
+console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
+console.log(addTwoNumbers([0], [0]));
+console.log(addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]));
